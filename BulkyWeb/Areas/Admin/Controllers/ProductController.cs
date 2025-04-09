44846997c1952c1b21e74cf5851ec5e6e141854c
J55E88 <jorge_es_ga@hotmail.com>
2025-04-09 15:22:52 -0600

    Added !ProductRepository to IUnitOfWork. Also CRUD operations with each view are working now

---
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,101 @@
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace BulkyWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            var objProductList = _unitOfWork.Product.GetAll().ToList();
+            return View(objProductList);
+        }
+
+        //-------------------------------------------------------------------------------------
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Add(product);
+                _unitOfWork.Save();
+                TempData["success"] = "Product created succesfully";
+                return RedirectToAction("Index","Product");
+            }
+            return View();
+        }
+
+
+        //--------------------------------------------------------------------------------------
+        public IActionResult Edit(int id)
+        {
+            if(id == null || id == 0)
+                return NotFound();
+
+            Product? product = _unitOfWork.Product.Get(u  => u.Id == id);
+
+            if (product == null)
+                return NotFound();
+
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Update(product);
+                _unitOfWork.Save();
+                TempData["success"] = "Product updated succesfully";
+                return RedirectToAction("Index", "Product");
+            }
+            return View();
+        }
+
+
+        //--------------------------------------------------------------------------------------
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+                return NotFound();
+
+            Product? product = _unitOfWork.Product.Get(u => u.Id == id);
+
+            if(product == null)
+                return NotFound();
+
+            return View(product);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            Product? product = _unitOfWork.Product.Get(u => u.Id == id);
+
+            if(product == null)
+                return NotFound();
+
+            _unitOfWork.Product.Remove(product);
+            _unitOfWork.Save();
+            TempData["success"] = "Product deleted succesfully";
+
+            return RedirectToAction("Index", "Product");
+        }
+    }
+}
